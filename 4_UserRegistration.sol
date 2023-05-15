04ea12f746d530260d8b43ee8c5b973633e9df56
nhat <98156733+nhatlieu@users.noreply.github.com>
2023-05-15 23:13:12 +0900

    Update 4_UserRegistration.sol
    Fees are added for exceeding the set number of username changes and comments explaining each code.

--- 4_UserRegistration.sol
+++ 4_UserRegistration.sol
@@ -1,30 +1,56 @@
 pragma solidity >=0.8.2 <0.9.0;
 
-contract userRegistration {
-
-    address owner;
-    mapping(address => uint) public registeredTimes;    
-    mapping(address => string) public username;
+contract UserRegistration {
+    address owner; // The owner of the contract
+    mapping(address => uint) public registeredTimes; // A mapping to keep track of the number of times a user has registered
+    mapping(address => string) public username; // A mapping to store the username of each registered user
     
+    uint256 fee = 10000000000000000; // Fee for changing name more than 5 times
+
     constructor() {
-         owner = msg.sender;
-         }
-    
-    function Registered() internal {
-        registeredTimes[msg.sender] ++;
-        }
-
-    function setUser(string memory _name) public {
-        require(registeredTimes[msg.sender] < 1);
-        username[msg.sender] = _name;
-        Registered();
+        owner = msg.sender;
     }
 
+    // Function to set or update a user's username
+    // Requirements:
+    // - User must not have registered or updated their username more than 4 times
+    function setUser(string memory _name) public payable {
+        
+        if (registeredTimes[msg.sender] > 6) {
+            require(msg.value >= fee, "Not enough Ether provided.");
+        }
+
+        username[msg.sender] = _name;
+        registeredTimes[msg.sender]++;
+    }
+
+    // Function to get the username of the caller
+    // Returns:
+    // - The username of the caller
     function myUsername() public view returns(string memory) {
         return(username[msg.sender]);
     }
 
+    // Function to get the number of times the caller has registered or updated their username
+    // Returns:
+    // - The number of times the caller has registered or updated their username
     function myRegisteredTimes() public view returns(uint) {
         return(registeredTimes[msg.sender]);
+    }
+
+    // Function to get the username of a specific user
+    // Params:
+    // - _address: The address of the user whose username we want to retrieve
+    // Returns:
+    // - The username of the user
+    function searchUsername(address _address) public view returns(string memory) {
+        return (username[_address]);
+    }
+
+    // Allows the contract owner to withdraw all Ether stored in the contract
+    function withdraw() public {
+        require(msg.sender == owner, "Only the contract owner can withdraw");
+        address payable ownerTransfer;
+        ownerTransfer.transfer(address(this).balance);
     }
 }
