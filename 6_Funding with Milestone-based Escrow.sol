5e6d2c36ca5ae99bfd38d10745ce622a7c4f6048
nhat <98156733+nhatlieu@users.noreply.github.com>
2023-05-19 00:51:41 +0900

    Update 6_Funding with Milestone-based Escrow.sol
    I added a refund function and a release function.

--- 6_Funding with Milestone-based Escrow.sol
+++ 6_Funding with Milestone-based Escrow.sol
@@ -5,35 +5,80 @@ import "./5_ProjectCreation.sol";
 contract Funding is ProjectCreation {
     event Funded(uint projectId, address contributor, uint amount);
 
-    mapping(address => mapping(uint => uint)) public balances; // Stores the balances of contributors for each project
-    mapping(address => mapping(address => mapping(uint => uint))) public contribution; // Stores the contribution amount of each contributor for each project
+    mapping(address => mapping(uint => uint)) public totalContribution;
+    mapping(address => mapping(address => mapping(uint => uint))) public contribution;
 
     function fundProject(address _owner, uint _projectId, uint _amount) public payable {
         require(msg.value == _amount, "Sent value does not match the input amount");
 
-        // Retrieve the projects owned by the given owner
         Project[] storage userProjects = projects[_owner];
 
         for(uint i = 0; i < userProjects.length; i++) {
-            // Check if the project matches the provided project ID and has not been deleted
             if(userProjects[i].projectId == _projectId && !userProjects[i].isDeleted) {
-                // Increase the total contribution of the project
                 userProjects[i].totalContribution += _amount;
-
-                // Update the balance of the contributor
-                balances[_owner][_projectId] += _amount;
-
-                // Update the contribution amount of the specific contributor for the project
+                totalContribution[_owner][_projectId] += _amount;
                 contribution[msg.sender][_owner][_projectId] += _amount;
-
-                // Emit an event to indicate that the project has been funded
                 emit Funded(_projectId, msg.sender, _amount);
-
                 return;
             }
         }
 
-        // Revert the transaction if the project is not found or has been deleted
         revert("Project not found or has been deleted");
+    }
+
+    function refund(address _owner, uint _projectId) public {
+        Project[] storage userProject = projects[_owner];
+        uint projectIndex;
+        bool projectFound = false;
+
+        for(uint i = 0; i < userProject.length; i++) {
+            if(userProject[i].projectId == _projectId) {
+                projectIndex = i;
+                projectFound = true;
+                break;
+            }
+        }
+
+        require(projectFound, "Project not found");
+        require(!userProject[projectIndex].isDeleted, "Project has been deleted");
+
+        if(block.timestamp > userProject[projectIndex].deadline && userProject[projectIndex].totalContribution < userProject[projectIndex].fundingGoal) {
+            uint refundAmount = contribution[msg.sender][_owner][_projectId];
+            require(refundAmount > 0, "No contribution or refund already claimed");
+
+            totalContribution[_owner][_projectId] -= refundAmount;
+            contribution[msg.sender][_owner][_projectId] = 0;
+            
+            payable(msg.sender).transfer(refundAmount);
+        }
+        else {
+            revert("Refund not possible");
+        }
+    }
+
+    function release(uint _projectId) public {
+        Project[] storage userProject = projects[msg.sender];
+        uint projectIndex;
+        bool projectFound = false;
+
+        for(uint i = 0; i < userProject.length; i++) {
+            if(userProject[i].projectId == _projectId) {
+                projectIndex = i;
+                projectFound = true;
+                break;
+            }
+        }
+
+        require(projectFound, "Project not found");
+        require(!userProject[projectIndex].isDeleted, "Project has been deleted");
+        require(block.timestamp > userProject[projectIndex].deadline, "Project deadline has not passed");
+
+        if (userProject[projectIndex].totalContribution >= userProject[projectIndex].fundingGoal) {
+        payable(msg.sender).transfer(userProject[projectIndex].totalContribution);
+        userProject[projectIndex].totalContribution = 0;
+        }
+        else {
+            revert("Funding goal not met, funds cannot be released");
+        }
     }
 }
